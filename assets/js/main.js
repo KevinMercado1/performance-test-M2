fa1a58e9bb107c628fa525e80878852c970d83f4
Kevin <kevinmercado1912000@icloud.com>
2026-04-27 09:46:12 -0500

    finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,11 @@
 const date = new Date()
 document.getElementById("year").innerText = date.getFullYear()
 
-// Write your Js code here 
+//In this place I put the function of button.//
+
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav");
+
+navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("nav--visible");
+});
